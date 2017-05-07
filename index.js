241c26537933f7a1d728314dff1410df2083af38
Erdem <erdem.celikten@gmail.com>
2017-05-07 23:55:03 +0200

    initial
    initial

--- index.js
+++ index.js
@@ -1,73 +1,116 @@
 /**
- * Created by fabianschweizer on 06.05.17.
+ * Created by Erdem Celikten & Fabian Schweizer on 06.05.17.
  */
 
 const apis = {
-    audio: null,
+    audio: [],
     battery: null
 };
 
-const audioSuccess = function(localMediaStream) {
-    var audioCtx = new AudioContext();
-    var audioSrc = audioCtx.createMediaStreamSource(localMediaStream);
-    var analyser = audioCtx.createAnalyser();
-    analyser.fftSize = 1024;
+const audioSuccess = function (localMediaStream) {
+    const audioCtx = new AudioContext();
+    const audioSrc = audioCtx.createMediaStreamSource(localMediaStream);
+    const analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 256;
     audioSrc.connect(analyser);
-    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-
-    var bins = [];
-    frequencyData.forEach(function(e) {
-        var e = document.createElement('div');
+    const frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    const bins = [];
+    let loop = true;
+    frequencyData.forEach(function (e) {
+        e = document.createElement('div');
         e.classList.add('bin');
         document.getElementById('bins').appendChild(e);
         bins.push(e);
     });
 
+    setTimeout(() => {
+        loop = false;
+    }, 1800);
 
     function renderFrame() {
         analyser.getByteFrequencyData(frequencyData);
-        apis.audio = frequencyData;
-
+        apis.audio.push(frequencyData);
         frequencyData.forEach(function (data, index) {
-            bins[index].style.height = ((data * 100) / 256) + "%";
+            let width = '1px';
+            if (data !== 0) {
+                width = ((data * 100) / 256) + '%';
+                // width = '400px';
+            }
+            bins[index].style.width = width;
         });
-
-        requestAnimationFrame(renderFrame);
+        if (loop) {
+            requestAnimationFrame(renderFrame);
+        } else {
+            gameoflife();
+        }
     }
+
     renderFrame();
 
 
+    function gameoflife() {
+        let cells = apis.audio;
+        setInterval(() => {
+            cells = apis.audio;
+            apis.audio = draw(cells.map((row, rowIdx) => {
+                return row.map((col, colIdx) => {
+                    return check(rowIdx, colIdx);
+                })
+            }));
+            console.log('loop');
+        },200);
+
+        function check(rI, cI) {
+            let alive = 0;
+            const neighbours = [
+                [rI - 1, cI - 1],
+                [rI - 1, cI],
+                [rI - 1, cI + 1],
+                [rI, cI - 1],
+                [rI, cI + 1],
+                [rI + 1, cI - 1],
+                [rI + 1, cI],
+                [rI + 1, cI + 1]
+            ];
+            neighbours.forEach((p) => {
+                if (cells[p[0]] !== undefined && cells[p[0]][p[1]] !== 0) {
+                    alive++;
+                }
+            });
+            if (alive === 2 || alive === 3) {
+                return 1;
+            }
+            return 0;
+        }
+
+        function draw(nc) {
+            const bins = document.getElementById('bins');
+            bins.innerHTML = '';
+            nc.forEach((row, rI) => {
+                const line = document.createElement('div');
+                const id = `row${rI}`;
+                line.id = id;
+                line.classList.add('row');
+                document.getElementById('bins').appendChild(line);
+                row.forEach((col) => {
+                    const dot = document.createElement('div');
+                    if (col === 1) {
+                        dot.classList.add('alive');
+                    }
+                    document.getElementById(id).appendChild(dot);
+                });
+            });
+            return nc;
+        }
+    }
+
+
 };
 
-const audioFail = function() {
+const audioFail = function () {
     console.log('Fail');
 };
 
-navigator.getUserMedia({audio: true}, function(localMediaStream) {audioSuccess(localMediaStream)}, audioFail);
-
-// const battery = navigator.getBattery();
-//
-// if (battery) {
-//     battery.then(
-//         (batteryManager) => {
-//             apis.battery = batteryManager;
-//
-//             const updateBattery = function(self, type) {
-//                 console.log(self, type);
-//             };
-//
-//             batteryManager.onchargingchange = function () {
-//                 updateBattery(this, 'charging');
-//             };
-//             batteryManager.onchargingtimechange = function () {
-//                 updateBattery(this, 'chargingtime');
-//             };
-//             batteryManager.ondischargingtimechange = function () {
-//                 updateBattery(this, 'dischargingtime');
-//             };
-//             batteryManager.onlevelchange = function () {
-//                 updateBattery(this, 'level');
-//             };
-//         }
-//     );
-// }
+navigator.getUserMedia({audio: true}, function (localMediaStream) {
+    audioSuccess(localMediaStream)
+}, audioFail);
